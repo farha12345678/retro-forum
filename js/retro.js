013789a306c56a02d5794ea0cb49df364d7d1b37
farha12345678 <sabikunnaharfarha01@gmail.com>
2024-03-04 00:13:04 +0600

    third commit

--- js/retro.js
+++ js/retro.js
@@ -7,8 +7,12 @@ const loadNews = async (catid) => {
         const div = document.createElement('div')
         div.innerHTML = `
     <div class="card lg:card-side bg-[#7A7DFC1A] mb-10 shadow-xl border border-[#7A7DFC] w-[700px] h-[300px] mt-10 ml-20  ">
-                    <figure><img src="${item.image}" alt="Album"/></figure>
-                    <div class="card-body ">
+    <div class="avatar">
+    <div class="w-16 h-16 rounded-xl mt-10 ml-10">
+      <img src="${item.image}" />
+    </div>
+    </div>
+                    <div class="card-body space-y-2">
                         <div class="flex">
                             <p>${item.category}</p>
                             <p>Author :<span>${item.author.name}</span></p>
@@ -31,23 +35,83 @@ const loadNews = async (catid) => {
                             </div>
                         </div>
                       <div class="card-actions justify-end">
-                        <button class="btn  rounded-full bg-[#10B981] text-white"><i class="fa-solid fa-envelope"></i></button>
+                        <button onclick="buttonClick()" id="btn-click" class="btn button  rounded-full bg-[#10B981] text-white"><i class="fa-solid fa-envelope"></i></button>
                       </div>
                     </div>
                   </div>
     
     `
-    newsContainer.appendChild(div)
+        newsContainer.appendChild(div)
     })
 
 }
 const handleSearch = () => {
     const value = document.getElementById('search-field').value;
-    if(value){
-loadNews(value)
+    if (value) {
+        loadNews(value)
     }
-    else{
+    else {
         alert("Please enter valid Id")
     }
 }
+const loadLatest = async () => {
+    const response = await fetch("https://openapi.programming-hero.com/api/retro-forum/latest-posts")
+    const data = await response.json()
+    const loadLatest = document.getElementById('latest-post')
+
+    data.forEach((item) => {
+        const div = document.createElement('div')
+        div.innerHTML = `
+        <div class="card w-96 h-[482px] bg-base-100 border border-[#12132D26] pl-10 gap-y-5">
+                    <figure class="px-8 pt-10">
+                      <img src="${item.cover_image}" alt="" class="rounded-xl" />
+                    </figure>
+                    <div class=" flex text-base font-normal gap-x-5">
+                        <p class="text-base font-normal "><i class="fa-regular fa-calendar"></i></p>
+                     <p class="text-base font-normal ">${item.author.posted_date}</p>
+                     </div>
+                      <div class="card-actions">
+                        <h4 class="text-lg font-extrabold">${item.title}</h4>
+                            <p class="text-base font-normal">${item.description} </p>
+                      </div>
+                      <div class="flex gap-x-10">
+                        <div class="avatar">
+                            <div class="w-10 rounded-full">
+                              <img src="${item.profile_image}" />
+                              
+                            </div>
+                        
+                          </div>
+                          <div class="grid">
+                            <h5 class="text-base font-bold">${item.author.name}</h5>
+                        <p class="text-base font-normal">${item.author.designation}</p>
+                        </div>
+                          
+                      </div>
+                    
+                  </div>
+        
+        `
+        loadLatest.appendChild(div)
+    })
+}
+// button
+function buttonClick (){
+    const selectedContainer = document.getElementById('slected-container')
+    const h4 = document.createElement('h4')
+    const icon = document.createElement('i')
+    const p = document.createElement('p')
+    h4.innerText = '${item.category}'
+    icon.innerText = '<i class="fa-regular fa-eye mr-2"></i>'
+    p.innerText = '${item.view_count}'
+
+       
+    }
+    
+buttonClick()
+
+    
+
+
 loadNews("")
+loadLatest()
